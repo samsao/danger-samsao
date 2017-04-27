b9a962f9d29ee9b8145e54a23bb9c81fe1f63d07
Matthieu Vachon <mvachon@samsao.co>
2017-04-27 15:57:42 -0400

    Initial commit

--- lib/samsao/plugin.rb
+++ lib/samsao/plugin.rb
@@ -1,23 +1,6 @@
 module Danger
-  # This is your plugin class. Any attributes or methods you expose here will
-  # be available from within your Dangerfile.
-  #
-  # To be published on the Danger plugins site, you will need to have
-  # the public interface documented. Danger uses [YARD](http://yardoc.org/)
-  # for generating documentation from your plugin source, and you can verify
-  # by running `danger plugins lint` or `bundle exec rake spec`.
-  #
-  # You should replace these comments with a public description of your library.
-  #
-  # @example Ensure people are well warned about merging on Mondays
-  #
-  #          my_plugin.warn_on_mondays
-  #
-  # @see  Matthieu Vachon/danger-samsao
-  # @tags monday, weekends, time, rattata
-  #
+  # Samsao's Danger plugin main class
   class DangerSamsao < Plugin
-
     # An attribute that you can read/write from your Dangerfile
     #
     # @return   [Array<String>]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,29 +1,21 @@
 require 'pathname'
 ROOT = Pathname.new(File.expand_path('../../', __FILE__))
-$:.unshift((ROOT + 'lib').to_s)
-$:.unshift((ROOT + 'spec').to_s)
+$LOAD_PATH.unshift((ROOT + 'lib').to_s)
+$LOAD_PATH.unshift((ROOT + 'spec').to_s)
 
 require 'bundler/setup'
-require 'pry'
 
-require 'rspec'
 require 'danger'
+require 'danger_plugin'
+require 'pry'
+require 'rspec'
 
-# Use coloured output, it's the best.
+# Configure RSpec
 RSpec.configure do |config|
-  config.filter_gems_from_backtrace "bundler"
+  config.filter_gems_from_backtrace 'bundler'
   config.color = true
   config.tty = true
 end
-
-require 'danger_plugin'
-
-# These functions are a subset of https://github.com/danger/danger/blob/master/spec/spec_helper.rb
-# If you are expanding these files, see if it's already been done ^.
-
-# A silent version of the user interface,
-# it comes with an extra function `.string` which will
-# strip all ANSI colours from the string.
 
 # rubocop:disable Lint/NestedMethodDefinition
 def testing_ui
@@ -34,21 +26,21 @@ def testing_ui
 
   cork = Cork::Board.new(out: @output)
   def cork.string
-    out.string.gsub(/\e\[([;\d]+)?m/, "")
+    out.string.gsub(/\e\[([;\d]+)?m/, '')
   end
+
   cork
 end
 # rubocop:enable Lint/NestedMethodDefinition
 
-# Example environment (ENV) that would come from
-# running a PR on TravisCI
+# Testing environment (ENV)
 def testing_env
   {
-    'HAS_JOSH_K_SEAL_OF_APPROVAL' => 'true',
-    'TRAVIS_PULL_REQUEST' => '800',
-    'TRAVIS_REPO_SLUG' => 'artsy/eigen',
-    'TRAVIS_COMMIT_RANGE' => '759adcbd0d8f...13c4dc8bb61d',
-    'DANGER_GITHUB_API_TOKEN' => '123sbdq54erfsd3422gdfio'
+    'BITRISE_IO' => 'true',
+    'BITRISE_PULL_REQUEST' => '92',
+    'DANGER_GITHUB_API_BASE_URL' => 'https://api.github.com',
+    'DANGER_GITHUB_API_TOKEN' => ENV['DANGER_GITHUB_API_TOKEN'],
+    'GIT_REPOSITORY_URL' => 'git@github.com:samsao/verdant.git'
   }
 end
 
